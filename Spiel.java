300c4457a3fbb01eaf96e8b169a51033a08d7433
Redheadher0 <jakob.pantin@iserv-gis.de>
2025-05-19 13:04:32 +0200

    mehr private

--- Spiel.java
+++ Spiel.java
@@ -2,6 +2,11 @@ public class Spiel {
   private byte dran;
   private byte phase;
   private byte zugNummer;
+  private Spieler[] mitSpieler;
+  private Kontinent[] kontinente;
+  private Land[] laender;
+  private RisikoGui gui;
+  
   public Spiel(String spielerName[], RisikoGui meineGui) {
     
   }
