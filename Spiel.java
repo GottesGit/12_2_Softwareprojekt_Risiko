fcd065d2f42bb17c80c21eae77d2a80e9b5a28e3
GottesGit <lasse.per.lippardt@iserv-gis.de>
2025-05-19 13:05:02 +0200

    Update Spiel.java

--- Spiel.java
+++ Spiel.java
@@ -3,6 +3,8 @@ public class Spiel {
   private byte phase;
   private byte zugNummer;
   public Spiel(String spielerName[], RisikoGui meineGui) {
-    
+    for (i = 0; i < spielerName[].length; i++) {
+      private Spieler;//array mitspieler müsste auf die Ruichtige Länge und dann miot Spielern gefüllt werden
+    }
   }
 }
