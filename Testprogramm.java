484beb73f371777fa31dbe0904f4bd0fdac4359b
GottesGit <lasse.per.lippardt@iserv-gis.de>
2025-05-20 13:49:25 +0200

    nix

--- Testprogramm.java
+++ Testprogramm.java
@@ -2,7 +2,7 @@
 public class Testprogramm {
   private static String[] laenderSvg = new String[2];
   public static void main(String[] args) {
-    test(laenderSvg);
+    test(["a", "b"]);
   }
   public static void test(String stringarray[]){
     System.out.println(stringarray[1]);
